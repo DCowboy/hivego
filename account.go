26f966bfeb48af7b6d95975699450a35c8e87a35
DCowboy <[email>
2024-09-23 22:53:12 -0400

    added account and key match verification

--- account.go
+++ account.go
@@ -1,8 +1,10 @@
 package hivego
 
+
 import (
-	//~ "fmt"
-	"strings"
+	"fmt"
+	
+	//~ "strings"
 	"encoding/json"
 )
 
@@ -10,24 +12,56 @@ var (
 	
 )
 
-type KeyReferences struct {
-	Accounts               []string     `json:"accounts"`
+
+
+func (h *HiveRpcNode) CheckAccount(account string) (string, error) {
+	query := hrpcQuery {
+		method: "condenser_api.lookup_accounts",
+		params: []any{account, 1},
+	}
+
+	response, err := h.rpcExec(h.address, query)
+	if err != nil {
+		return "", fmt.Errorf("response err: %s", err)
+	}
+
+	var accts []interface{}
+	if uErr := json.Unmarshal(response, &accts); uErr != nil {
+		return "", fmt.Errorf("Unmarshal err: %s", uErr)
+	}
+	if accts[0].(string) == account {
+		return accts[0].(string), nil
+	} else {
+		return "", fmt.Errorf("Account Not Found.")
+	}
 }
 
-func (h *HiveRpcNode) checkAccount(wif *string) (string, error) {
-	query := hrpcQuery{
-		method: "account_by_key_api.get_key_references",
-		params: map[string]string{
-			"keys": [*wif],
-		},
-	}
-	res, err := h.rpcExec(h.address, query)
+func (h *HiveRpcNode) CheckKey(wif *string, account string) (bool, error) {
+	//get key pair from private key
+	keyPair, err := KeyPairFromWif(*wif)
 	if err != nil {
-		return nil, err
+		return false, err
 	}
-	accts := &KeyReferences{}
+	//decode public key to string
+	pubKey := GetPublicKeyString(keyPair.PublicKey)
+	//get account name from public key
+	query := hrpcQuery {
+		method: "condenser_api.get_key_references",
+		params: []any{[]string{*pubKey}},
+	}
+	response, err := h.rpcExec(h.address, query)
+	if err != nil {
+		return false, fmt.Errorf("response err: %s", err)
+	}
+	var accts []interface{}
 	if uErr := json.Unmarshal(response, &accts); uErr != nil {
-		return nil, uErr
+		return false, fmt.Errorf("Unmarshal err: %s", uErr)
 	}
-	return accts, nil
+	//untangle response and see if it matches expected account
+	holder := accts[0].([]interface{})
+	if holder[0].(string) == account {
+		return true, nil
+	} else {
+		return false, fmt.Errorf("Key does not match account.")
+	}
 }
